d6c8af055d5505296d4c307e9b668e427249ad3a
pcanwar <aalruwai@stevens.edu>
2023-02-26 23:55:42 -0500

    fixed the list artwork func, need to keep the reqand use mux package

--- artwork/handlers/errors.go
+++ artwork/handlers/errors.go
@@ -1,5 +1,20 @@
 package handlers
 
+var (
+	ErrorInvalidAddress          = "invalid address"
+	ErrorAddressAlreadyExist     = "address already exist"
+	ErrorFailedToFetchRecords    = "failed to fetch records"
+	ErrorFailedToUnmarshalRecord = "failed to unmarshal record"
+	ErrorFailedToMarshalItem     = "failed to marshal item"
+	ErrorInvalidData             = "invalid data"
+	ErrorUnableToDealeteItem     = "unable to dealete item"
+	ErrorUnableToFindItem        = "unable to find item"
+	ErrorUnableToUpdateItem      = "unable to update item"
+	ErrorUnableDynamodbToPutItem = "unable to dynamodb to put item"
+	ErrorAddressAlreadyExists    = "address already exists"
+	ErrorAddressNotFound         = "address not found"
+)
+
 var methodNotAllowed = "method not allowed"
 
 type Errors struct {
--- artwork/handlers/listArtwork.go
+++ artwork/handlers/listArtwork.go
@@ -1,12 +1,20 @@
 package handlers
 
 import (
+	"errors"
+
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/gorilla/mux"
 )
 
 func List(req events.APIGatewayProxyRequest, tableName string, dyna dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
 	// Create the key for the DynamoDB GetItem operation.
+	ownerAddress := mux.Vars(req)["ownerAddress"]
+	artworkID := mux.Vars(req)["artworkID"]
 	key := map[string]*dynamodb.AttributeValue{
 		"address": {
 			S: aws.String(ownerAddress),
@@ -17,29 +25,26 @@ func List(req events.APIGatewayProxyRequest, tableName string, dyna dynamodbifac
 	}
 
 	input := &dynamodb.GetItemInput{
-        Key:       key,
-        TableName: aws.String(tableName),
-    }
+		Key:       key,
+		TableName: aws.String(tableName),
+	}
 
 	output, err := dyna.GetItem(input)
 
-    if err != nil {
-        return nil, errors.New(ErrorFailedToFetchRecords)
-    }
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecords)
+	}
 
 	if len(output.Item) == 0 || output.Item == nil {
-        return nil, nil
-    }
+		return nil, nil
+	}
 
-    artwork := new(Artwork)
-    err = dynamodbattribute.UnmarshalMap(output.Item, artwork)
+	artwork := new(Artwork)
+	err = dynamodbattribute.UnmarshalMap(output.Item, artwork)
 
-    if err != nil {
-        return nil, errors.New(ErrorFailedToUnmarshalRecord)
-    }
+	if err != nil {
+		return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
 
-    return artwork, nil
-}
-
-
+	return artwork, nil
 }
